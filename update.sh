6e0ee7f766801091d9e322e24196edb2dae5f2b6
campisano <riccardo.campisano@linea.gov.br>
2016-07-08 14:04:14 -0300

    update.sh updated correcting error codes and pip verbosity

--- update.sh
+++ update.sh
@@ -10,29 +10,46 @@ function error_exit {
     exit "${2:-1}"
 }
 
+echo
 echo "= Updating the code ="
 cd $DRI_HOME
 git pull || error_exit "Error, exit" 1
 mkdir -p masterlist/static/django || error_exit "Error, exit" 2
+
+echo
 echo "= Updating the images ="
 test -d masterlist/static/img || git clone https://github.com/linea-it/masterlist_img.git masterlist/static/img || error_exit "Error, exit" 3
 cd masterlist/static/img  || error_exit "Error, exit" 4
 git pull  || error_exit "Error, exit" 5
 cd ../../..
+
+echo
 echo "= Setting up env ="
-source env/bin/activate || error_exit "Error, exit" 2
+source env/bin/activate || error_exit "Error, exit" 6
+
+echo
 echo "= Updating requirements ="
-pip3 install -r requirements.txt || error_exit "Error, exit" 3
+pip3 install -U -r requirements.txt || error_exit "Error, exit" 7
+
+echo
 echo "= Entering in the application folder ="
-cd masterlist  || error_exit "Error, exit" 4
+cd masterlist  || error_exit "Error, exit" 8
+
+echo
 echo "= Updating the database structure ="
-python manage.py migrate || error_exit "Error, exit" 5
+python manage.py migrate || error_exit "Error, exit" 9
+
+echo
 echo "= Importing the database data ="
-python manage.py loaddata initial_data.json || error_exit "Error, exit" 6
+python manage.py loaddata initial_data.json || error_exit "Error, exit" 10
+
+echo
 echo "= Cleaning up the auto-generated static files ="
-python manage.py collectstatic --clear --noinput || error_exit "Error, exit" 7
+python manage.py collectstatic --clear --noinput --verbosity 0 || error_exit "Error, exit" 11
 cd ..
 ### REMEMBER TO IMPLEMENT A WAY TO apache reload
+
+echo
 echo "= Exiting ="
 deactivate' && exit 0 || exit 127
 
