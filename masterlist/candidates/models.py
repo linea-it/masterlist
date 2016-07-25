74ac762f3dd9d0e74605b673890a49798c6155c8
bnord <briandnord@gmail.com>
2016-07-25 13:00:45 -0500

    make migrations and model updates

--- masterlist/candidates/models.py
+++ masterlist/candidates/models.py
@@ -11,9 +11,11 @@ class Candidate(models.Model):
     thumb = models.CharField(null=True, max_length=255, verbose_name="Image")
     id = models.IntegerField(null=False, verbose_name="ID", primary_key=True)
     object_id = models.CharField(null=True, max_length=128, verbose_name="Object ID")
+    rank = models.IntegerField(null=True, verbose_name="Rank")
     ra = models.FloatField(null=False, verbose_name="RA")
     dec = models.FloatField(null=False, verbose_name="Dec")
-    rank = models.IntegerField(null=True, verbose_name="Rank")
+    ra_field = models.FloatField(null=True, verbose_name="RA Field")
+    dec_field = models.FloatField(null=True, verbose_name="Dec Field")
     mag_min = models.FloatField(null=True, verbose_name="Mag Min")
     mag_max = models.FloatField(null=True, verbose_name="Mag Max")
     vis_start = models.CharField(null=True, max_length=64, verbose_name="Vis Start")
@@ -23,6 +25,10 @@ class Candidate(models.Model):
     followup_date = models.CharField(null=True,max_length=64,  verbose_name="followup date")
     followup_facility = models.CharField(null=True,max_length=64,  verbose_name="followup facility")
     followup_success = models.CharField(null=True,max_length=64,  verbose_name="followup success")
+    z_spec_src = models.FloatField(null=True, verbose_name="Z spec source ")
+    z_phot_lens = models.FloatField(null=True, verbose_name="Z photo lens")
+    lens_class = models.CharField(null=True,max_length=64,  verbose_name="Lens (Y/N)")
+    system_type = models.CharField(null=True,max_length=64,  verbose_name="Type of Candidate (gal or qso)")
 
 
     def __str__(self):
